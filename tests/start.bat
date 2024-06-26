08df161029af2247968caf0cc1a1453ec0e35d7f
groza_1337 <groza_1337@mail.ru>
2024-06-26 06:32:55 +0300

    Дописал тесты для корректного ввода

--- tests/start.bat
+++ tests/start.bat
@@ -176,4 +176,251 @@ echo.  >> %file%
 
 echo ------------------->> %file%
 
+echo ---------------------------------------- >> %file%
+echo ------------- NORMAL TESTS ------------- >> %file%
+echo ---------------------------------------- >> %file%
+
+set num=01
+echo.  >> %file%
+echo input%num%.txt - Три стены одинаковой высоты >> %file%
+echo.  >> %file%
+
+echo ____________input%num%.txt____________ >> %file%
+type .\Input\input%num%.txt >> %file%
+echo.  >> %file%
+echo ___________________________________ >> %file%
+echo.  >> %file%
+
+echo Console output: (should be empty) >> %file%
+"%program%" ".\Input\input%num%.txt" ".\Output\output%num%.txt" >> %file% 2>&1
+echo.  >> %file%
+
+if exist .\Output\output%num%.txt (
+    echo ____________output%num%.txt____________ >> %file%
+    type .\Output\output%num%.txt >> %file%
+    echo.  >> %file%
+    echo ___________________________________ >> %file%
+) else (
+    echo File output%num%.txt wasn't created. >> %file%
+)
+echo.  >> %file%
+
+echo ------------------->> %file%
+
+set num=02
+echo.  >> %file%
+echo input%num%.txt - Стены идут лесенкой. >> %file%
+echo.  >> %file%
+
+echo ____________input%num%.txt____________ >> %file%
+type .\Input\input%num%.txt >> %file%
+echo.  >> %file%
+echo ___________________________________ >> %file%
+echo.  >> %file%
+
+echo Console output: (should be empty) >> %file%
+"%program%" ".\Input\input%num%.txt" ".\Output\output%num%.txt" >> %file% 2>&1
+echo.  >> %file%
+
+if exist .\Output\output%num%.txt (
+    echo ____________output%num%.txt____________ >> %file%
+    type .\Output\output%num%.txt >> %file%
+    echo.  >> %file%
+    echo ___________________________________ >> %file%
+) else (
+    echo File output%num%.txt wasn't created. >> %file%
+)
+echo.  >> %file%
+
+echo ------------------->> %file%
+
+set num=03
+echo.  >> %file%
+echo input%num%.txt - Между стенами есть одна яма. >> %file%
+echo.  >> %file%
+
+echo ____________input%num%.txt____________ >> %file%
+type .\Input\input%num%.txt >> %file%
+echo.  >> %file%
+echo ___________________________________ >> %file%
+echo.  >> %file%
+
+echo Console output: (should be empty) >> %file%
+"%program%" ".\Input\input%num%.txt" ".\Output\output%num%.txt" >> %file% 2>&1
+echo.  >> %file%
+
+if exist .\Output\output%num%.txt (
+    echo ____________output%num%.txt____________ >> %file%
+    type .\Output\output%num%.txt >> %file%
+    echo.  >> %file%
+    echo ___________________________________ >> %file%
+) else (
+    echo File output%num%.txt wasn't created. >> %file%
+)
+echo.  >> %file%
+
+echo ------------------->> %file%
+
+set num=04
+echo.  >> %file%
+echo input%num%.txt - Между стенами 2 ямы. >> %file%
+echo.  >> %file%
+
+echo ____________input%num%.txt____________ >> %file%
+type .\Input\input%num%.txt >> %file%
+echo.  >> %file%
+echo ___________________________________ >> %file%
+echo.  >> %file%
+
+echo Console output: (should be empty) >> %file%
+"%program%" ".\Input\input%num%.txt" ".\Output\output%num%.txt" >> %file% 2>&1
+echo.  >> %file%
+
+if exist .\Output\output%num%.txt (
+    echo ____________output%num%.txt____________ >> %file%
+    type .\Output\output%num%.txt >> %file%
+    echo.  >> %file%
+    echo ___________________________________ >> %file%
+) else (
+    echo File output%num%.txt wasn't created. >> %file%
+)
+echo.  >> %file%
+
+echo ------------------->> %file%
+
+set num=05
+echo.  >> %file%
+echo input%num%.txt - Две стены. >> %file%
+echo.  >> %file%
+
+echo ____________input%num%.txt____________ >> %file%
+type .\Input\input%num%.txt >> %file%
+echo.  >> %file%
+echo ___________________________________ >> %file%
+echo.  >> %file%
+
+echo Console output: (should be empty) >> %file%
+"%program%" ".\Input\input%num%.txt" ".\Output\output%num%.txt" >> %file% 2>&1
+echo.  >> %file%
+
+if exist .\Output\output%num%.txt (
+    echo ____________output%num%.txt____________ >> %file%
+    type .\Output\output%num%.txt >> %file%
+    echo.  >> %file%
+    echo ___________________________________ >> %file%
+) else (
+    echo File output%num%.txt wasn't created. >> %file%
+)
+echo.  >> %file%
+
+echo ------------------->> %file%
+
+set num=06
+echo.  >> %file%
+echo input%num%.txt - Одна стена. >> %file%
+echo.  >> %file%
+
+echo ____________input%num%.txt____________ >> %file%
+type .\Input\input%num%.txt >> %file%
+echo.  >> %file%
+echo ___________________________________ >> %file%
+echo.  >> %file%
+
+echo Console output: (should be empty) >> %file%
+"%program%" ".\Input\input%num%.txt" ".\Output\output%num%.txt" >> %file% 2>&1
+echo.  >> %file%
+
+if exist .\Output\output%num%.txt (
+    echo ____________output%num%.txt____________ >> %file%
+    type .\Output\output%num%.txt >> %file%
+    echo.  >> %file%
+    echo ___________________________________ >> %file%
+) else (
+    echo File output%num%.txt wasn't created. >> %file%
+)
+echo.  >> %file%
+
+echo ------------------->> %file%
+
+set num=07
+echo.  >> %file%
+echo input%num%.txt - Обратная пирамида из стен. >> %file%
+echo.  >> %file%
+
+echo ____________input%num%.txt____________ >> %file%
+type .\Input\input%num%.txt >> %file%
+echo.  >> %file%
+echo ___________________________________ >> %file%
+echo.  >> %file%
+
+echo Console output: (should be empty) >> %file%
+"%program%" ".\Input\input%num%.txt" ".\Output\output%num%.txt" >> %file% 2>&1
+echo.  >> %file%
+
+if exist .\Output\output%num%.txt (
+    echo ____________output%num%.txt____________ >> %file%
+    type .\Output\output%num%.txt >> %file%
+    echo.  >> %file%
+    echo ___________________________________ >> %file%
+) else (
+    echo File output%num%.txt wasn't created. >> %file%
+)
+echo.  >> %file%
+
+echo ------------------->> %file%
+
+set num=08
+echo.  >> %file%
+echo input%num%.txt - Пирамида из стен. >> %file%
+echo.  >> %file%
+
+echo ____________input%num%.txt____________ >> %file%
+type .\Input\input%num%.txt >> %file%
+echo.  >> %file%
+echo ___________________________________ >> %file%
+echo.  >> %file%
+
+echo Console output: (should be empty) >> %file%
+"%program%" ".\Input\input%num%.txt" ".\Output\output%num%.txt" >> %file% 2>&1
+echo.  >> %file%
+
+if exist .\Output\output%num%.txt (
+    echo ____________output%num%.txt____________ >> %file%
+    type .\Output\output%num%.txt >> %file%
+    echo.  >> %file%
+    echo ___________________________________ >> %file%
+) else (
+    echo File output%num%.txt wasn't created. >> %file%
+)
+echo.  >> %file%
+
+echo ------------------->> %file%
+
+set num=09
+echo.  >> %file%
+echo input%num%.txt - Стены со случайными высотами. >> %file%
+echo.  >> %file%
+
+echo ____________input%num%.txt____________ >> %file%
+type .\Input\input%num%.txt >> %file%
+echo.  >> %file%
+echo ___________________________________ >> %file%
+echo.  >> %file%
+
+echo Console output: (should be empty) >> %file%
+"%program%" ".\Input\input%num%.txt" ".\Output\output%num%.txt" >> %file% 2>&1
+echo.  >> %file%
+
+if exist .\Output\output%num%.txt (
+    echo ____________output%num%.txt____________ >> %file%
+    type .\Output\output%num%.txt >> %file%
+    echo.  >> %file%
+    echo ___________________________________ >> %file%
+) else (
+    echo File output%num%.txt wasn't created. >> %file%
+)
+echo.  >> %file%
+
+echo ------------------->> %file%
+
 echo -------- END OF FILE -------- >> %file%
