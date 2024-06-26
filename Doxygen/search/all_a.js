81dea784252ee4b83cf8c2612331dc9f99059b08
groza_1337 <groza_1337@mail.ru>
2024-06-26 09:34:10 +0300

    Doxygen обновил

--- Doxygen/search/all_a.js
+++ Doxygen/search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['toomanynumbersinfile_0',['TooManyNumbersInFile',['../_header_8h.html#ab0df38968e4f03a3f1f6d6df0f31f45aacc6ea1f65f5f43312b5a192760e35128',1,'Header.h']]],
-  ['type_1',['type',['../struct_error_info.html#a5aaa4d70e620a393da1fd0f062547aa9',1,'ErrorInfo']]]
+  ['thecalculationofwatervolumebetweenwalls_0',['Документация для программы &quot;TheCalculationOfWaterVolumeBetweenWalls&quot;',['../index.html',1,'']]],
+  ['toomanynumbersinfile_1',['TooManyNumbersInFile',['../_header_8h.html#ab0df38968e4f03a3f1f6d6df0f31f45aacc6ea1f65f5f43312b5a192760e35128',1,'Header.h']]],
+  ['type_2',['type',['../struct_error_info.html#a5aaa4d70e620a393da1fd0f062547aa9',1,'ErrorInfo']]]
 ];
--- Doxygen/search/searchdata.js
+++ Doxygen/search/searchdata.js
@@ -1,12 +1,13 @@
 var indexSectionsWithContent =
 {
-  0: "cdefghmnortw",
+  0: "cdefghmnortwдп",
   1: "e",
   2: "hm",
   3: "cdegmorw",
   4: "dt",
   5: "e",
-  6: "fmnot"
+  6: "fmnot",
+  7: "tдп"
 };
 
 var indexSectionNames =
@@ -17,7 +18,8 @@ var indexSectionNames =
   3: "functions",
   4: "variables",
   5: "enums",
-  6: "enumvalues"
+  6: "enumvalues",
+  7: "pages"
 };
 
 var indexSectionLabels =
@@ -28,6 +30,7 @@ var indexSectionLabels =
   3: "Функции",
   4: "Переменные",
   5: "Перечисления",
-  6: "Элементы перечислений"
+  6: "Элементы перечислений",
+  7: "Страницы"
 };
 
