fc2ec13b4c8d6f4cafaf387d22b2606ce690d646
groza_1337 <groza_1337@mail.ru>
2024-06-26 04:54:18 +0300

    Сделал скрипты для переноса .exe в папку Executable

--- TheCalculationOfWaterVolumeBetweenWalls/build.sh
+++ TheCalculationOfWaterVolumeBetweenWalls/build.sh
@@ -1,2 +1,5 @@
 rm -rf build && echo "Папка старого билда удалена"
-(mkdir build && cd build && cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake .. && make) && ./cleanup.sh && echo "Билд готов!"
+(mkdir build && cd build && cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake .. && make) &&
+./cleanup.sh &&
+./copy_exe_to_executable_folder.sh &&
+echo "Билд готов"
